796f6fdf7cdb829c01480f70b33fa3e452a92b71
Shane Nguyen <sn103103@gmail.com>
2023-05-01 02:10:54 -0700

    engine minigame

--- Assets/Minigames/Engine/EngineMinigame.cs
+++ Assets/Minigames/Engine/EngineMinigame.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
 
-public class EngineMinigame : Minigame, IPointerDownHandler
+public class EngineMinigame : Minigame, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private TextMeshPro clickCountText;
+    [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private int clicksRequired;
+    [SerializeField] private Sprite neutralSprite;
+    [SerializeField] private Sprite hitSprite;
     
     private int clickCount = 0;
 
@@ -13,12 +16,19 @@ public class EngineMinigame : Minigame, IPointerDownHandler
     {
         clickCount++;
         clickCountText.text = (clicksRequired - clickCount).ToString();
+        spriteRenderer.sprite = hitSprite;
         if (clickCount >= clicksRequired) WinMinigame();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        spriteRenderer.sprite = neutralSprite;
     }
 
     protected override void ResetState()
     {
         clickCount = 0;
         clickCountText.text = clicksRequired.ToString();
+        spriteRenderer.sprite = neutralSprite;
     }
 }
