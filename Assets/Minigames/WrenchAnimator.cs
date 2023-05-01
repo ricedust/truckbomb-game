b4327e267102d3ed7c27a935a6e5b4f19d5ef412
Shane Nguyen <sn103103@gmail.com>
2023-05-01 01:35:52 -0700

    added wheel minigame

---
+++ Assets/Minigames/WrenchAnimator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WrenchAnimator : MonoBehaviour, IPointerMoveHandler
+{
+    [SerializeField] private Transform wrench;
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        // get origin of wheel in screen space
+        Vector2 screenSpaceOrigin = Camera.main.WorldToScreenPoint(transform.position);
+        
+        // get angle between mouse and wheel origin
+        float mousePivotAngle = Vector2.SignedAngle(Vector2.down, eventData.position - screenSpaceOrigin);
+        
+        // set wrench angle
+        wrench.localEulerAngles = Vector3.forward * mousePivotAngle;
+    }
+}
